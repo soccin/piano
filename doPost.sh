408d36b1acd8f9c34c468a8aa93ed2336e15d022
Nicholas D. Socci <soccin@mskcc.org>
2024-04-21 14:17:07 -0400

    Version doPost.sh

--- doPost.sh
+++ doPost.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 set -ue
-export SDIR="$( cd "$( dirname "$0" )" && pwd )"
-R
 
+export SDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+PROJNO=$(echo $(pwd -P) | tr '/' '\n' | fgrep Proj_)
+
+echo -e "\nPhase-I: Get OncoKb Annotations\n"
+Rscript $SDIR/R/getPreAnnotationFile.R
+$SDIR/bin/runOncoKb.sh ${PROJNO}__UniqueFusions.tsv
+
+
+echo -e "\nPhase-II: Generate fusion report\n"
+Rscript $SDIR/R/reportMetaFusion.R
