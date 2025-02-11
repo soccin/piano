0a393f4d6d8275e4c0a98ebe9e37509d3394d06c
Nicholas D. Socci <soccin@mskcc.org>
2025-02-11 17:28:20 -0500

    fix(oncokb): update relative paths for virtual environment and annotator

--- bin/runOncoKb.sh
+++ bin/runOncoKb.sh
@@ -6,8 +6,8 @@ SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 INPUT=$1
 
-. $SDIR/ve.oncokb/bin/activate
-python3 $SDIR/oncokb-annotator/FusionAnnotator.py \
+. $SDIR/../ve.oncokb/bin/activate
+python3 $SDIR/../oncokb-annotator/FusionAnnotator.py \
     -b 0f2476ae-6157-44a8-9f36-56718dce7864 \
     -i $INPUT \
     -o ${INPUT/.tsv/.oncokb.tsv}
