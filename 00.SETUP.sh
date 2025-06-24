7a1bc423ce36559caf05f0898d73e90b8191b030
Nicholas D. Socci <soccin@mskcc.org>
2025-06-24 12:46:01 -0400

    feat: add setup script for Nextflow and oncokb-annotator installation
    - Introduce 00.SETUP.sh to automate the installation of Nextflow and the oncokb-annotator package.
    - Replace the previous INSTALL.oncokb file with a streamlined setup process using wget and tar for package retrieval.
    - Set up a Python virtual environment and install required dependencies from the oncokb-annotator repository.

---
+++ 00.SETUP.sh
@@ -0,0 +1,33 @@
+#
+# Piano SETUP
+#
+
+#
+# First install nextflow
+#
+
+cd bin
+curl -s https://get.nextflow.io | bash
+cd ..
+
+#
+# No longer cloning
+# Pulling in a specific release and tracking
+# it inside our repo
+#
+# git clone https://github.com/oncokb/oncokb-annotator.git
+#
+
+wget https://github.com/oncokb/oncokb-annotator/archive/refs/tags/v3.4.1.tar.gz
+tar xvfz v3.4.1.tar.gz
+mv oncokb-annotator-3.4.1 oncokb-annotator
+rm v3.4.1.tar.gz
+
+python3 -m venv ve.oncokb
+. ve.oncokb/bin/activate
+pip install --upgrade pip
+cd oncokb-annotator/
+pip install -r requirements/common.txt -r requirements/pip3.txt
+cd ..
+deactivate
+
